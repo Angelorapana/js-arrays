7fe976f64b340c95c9535e9c7882758eb074992a
angelorapana <angelorapana@icloud.com>
2025-12-10 23:59:29 +0100

    esercizio 5 unione elementi e separazione con virgola

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -32,4 +32,6 @@ if (isFabioPresent !== -1) {
 
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+
+console.log(teachersString);
