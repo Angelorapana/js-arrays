5d74d0e32899dc1d6866337ab6376a3bbb522ad4
angelorapana <angelorapana@icloud.com>
2025-12-10 23:47:42 +0100

    verifica presenza e salvataggio di un elemento dell'array

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -22,7 +22,14 @@ teachers.splice(1, 1);
 console.log(teachers);
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf("Fabio");
+
+if (isFabioPresent !== -1) {
+  
+  console.log("Insegnante Fabio è presente nell'array");}
+
+    else {console.log("Fabio non c'è, è andato via");}
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
